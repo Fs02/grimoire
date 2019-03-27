75e2e5abfbdce71f3de9cb8f92f7ed6c3cd57d63
kevinalfianto <kevinalfianto@gmail.com>
2019-03-27 15:39:20 +0700

    add escape string all

--- changeset/escape_string.go
+++ changeset/escape_string.go
@@ -8,3 +8,10 @@ import (
 func EscapeString(ch *Changeset, field string) {
 	ApplyString(ch, field, html.EscapeString)
 }
+
+// EscapeString escapes special characters like "<" to become "&lt;" for all changes on changeset. this is helper for html.EscapeString
+func EscapeStringAll(ch *Changeset) {
+	for field, _ := range ch.changes {
+		ApplyString(ch, field, html.EscapeString)
+	}
+}
--- changeset/escape_string_test.go
+++ changeset/escape_string_test.go
@@ -22,3 +22,25 @@ func TestEscapeString(t *testing.T) {
 
 	assert.Equal(t, "&#34;Fran &amp; Freddie&#39;s Diner&#34; &lt;tasty@example.com&gt;", ch.Changes()["name"])
 }
+
+func TestEscapeStringAll(t *testing.T) {
+	type User struct {
+		Name     string
+		Username string
+		Number   int
+	}
+
+	user := User{}
+	input := params.Map{
+		"name":     `"Fran & Freddie's Diner" <tasty@example.com>`,
+		"username": `"Fran & Freddie's Diner" <tasty@example.com>`,
+		"number":   `1`,
+	}
+
+	ch := Cast(user, input, []string{"name", "username", "number"})
+	EscapeStringAll(ch)
+
+	assert.Equal(t, "&#34;Fran &amp; Freddie&#39;s Diner&#34; &lt;tasty@example.com&gt;", ch.Changes()["name"])
+	assert.Equal(t, "&#34;Fran &amp; Freddie&#39;s Diner&#34; &lt;tasty@example.com&gt;", ch.Changes()["username"])
+	assert.Equal(t, 1, ch.Changes()["number"])
+}
