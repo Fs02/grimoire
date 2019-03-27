0a06068d626eada5e4a9cc132860abcb9300cd46
kevinalfianto <kevinalfianto@gmail.com>
2019-03-27 21:52:16 +0700

    change Escape string into variadic variable

--- changeset/escape_string.go
+++ changeset/escape_string.go
@@ -5,13 +5,8 @@ import (
 )
 
 // EscapeString escapes special characters like "<" to become "&lt;". this is helper for html.EscapeString
-func EscapeString(ch *Changeset, field string) {
-	ApplyString(ch, field, html.EscapeString)
-}
-
-// EscapeString escapes special characters like "<" to become "&lt;" for all changes on changeset. this is helper for html.EscapeString
-func EscapeStringAll(ch *Changeset) {
-	for field, _ := range ch.changes {
+func EscapeString(ch *Changeset, fields ...string) {
+	for _, field := range fields {
 		ApplyString(ch, field, html.EscapeString)
 	}
 }
--- changeset/escape_string_test.go
+++ changeset/escape_string_test.go
@@ -9,38 +9,19 @@ import (
 
 func TestEscapeString(t *testing.T) {
 	type User struct {
-		Name string
-	}
-
-	user := User{}
-	input := params.Map{
-		"name": `"Fran & Freddie's Diner" <tasty@example.com>`,
-	}
-
-	ch := Cast(user, input, []string{"name"})
-	EscapeString(ch, "name")
-
-	assert.Equal(t, "&#34;Fran &amp; Freddie&#39;s Diner&#34; &lt;tasty@example.com&gt;", ch.Changes()["name"])
-}
-
-func TestEscapeStringAll(t *testing.T) {
-	type User struct {
 		Name     string
 		Username string
-		Number   int
 	}
 
 	user := User{}
 	input := params.Map{
 		"name":     `"Fran & Freddie's Diner" <tasty@example.com>`,
 		"username": `"Fran & Freddie's Diner" <tasty@example.com>`,
-		"number":   `1`,
 	}
 
-	ch := Cast(user, input, []string{"name", "username", "number"})
-	EscapeStringAll(ch)
+	ch := Cast(user, input, []string{"name"})
+	EscapeString(ch, "name", "username")
 
 	assert.Equal(t, "&#34;Fran &amp; Freddie&#39;s Diner&#34; &lt;tasty@example.com&gt;", ch.Changes()["name"])
 	assert.Equal(t, "&#34;Fran &amp; Freddie&#39;s Diner&#34; &lt;tasty@example.com&gt;", ch.Changes()["username"])
-	assert.Equal(t, 1, ch.Changes()["number"])
 }
