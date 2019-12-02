76268ef42284a46f03d6d1eed37416f9dddbc650
Husni Munaya <husnimun@gmail.com>
2019-12-02 20:56:56 +0700

    Fix validateRequired

--- changeset/validate_required_test.go
+++ changeset/validate_required_test.go
@@ -1,8 +1,11 @@
 package changeset
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
+	"github.com/Fs02/grimoire/params"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -21,6 +24,22 @@ func TestValidateRequired(t *testing.T) {
 	assert.Nil(t, ch.Errors())
 }
 
+func TestValidateRequired_cast(t *testing.T) {
+	type customString string
+	type customType struct {
+		Field1    customString
+		Field2    customString
+		Field3    customString
+		FieldTime time.Time
+	}
+
+	ct := customType{Field1: "field1", FieldTime: time.Now()}
+	ch := Cast(ct, params.Map{"field2": "field2", "field3": "field3"}, []string{"field1", "field2", "field3", "field_time"})
+	ValidateRequired(ch, []string{"field1", "field2", "field3", "field_time"})
+
+	assert.Nil(t, ch.Errors())
+}
+
 func TestValidateRequired_error(t *testing.T) {
 	ch := &Changeset{
 		changes: map[string]interface{}{
@@ -34,6 +53,7 @@ func TestValidateRequired_error(t *testing.T) {
 	assert.Equal(t, 3, len(ch.Errors()))
 	assert.Equal(t, "field1 is required", ch.Errors()[0].Error())
 	assert.Equal(t, "field2 is required", ch.Errors()[1].Error())
+	assert.Equal(t, "field3 is required", ch.Errors()[2].Error())
 
 	// empty struct
 	ch = &Changeset{
@@ -52,4 +72,24 @@ func TestValidateRequired_error(t *testing.T) {
 	assert.Equal(t, 2, len(ch.Errors()))
 	assert.Equal(t, "field2 is required", ch.Errors()[0].Error())
 	assert.Equal(t, "field3 is required", ch.Errors()[1].Error())
+}
+
+func TestValidateRequired_cast_error(t *testing.T) {
+	type customString string
+	type customType struct {
+		Field1    customString
+		Field2    customString
+		Field3    customString
+		FieldTime time.Time
+	}
+
+	ct := customType{Field1: "field1"}
+	fmt.Println(ct.FieldTime)
+	ch := Cast(ct, params.Map{"field2": "field2"}, []string{"field1", "field2", "field3", "field_time"})
+	ValidateRequired(ch, []string{"field1", "field2", "field3", "field_time"})
+
+	assert.NotNil(t, ch.Errors())
+	assert.Equal(t, 2, len(ch.Errors()))
+	assert.Equal(t, "field3 is required", ch.Errors()[0].Error())
+	assert.Equal(t, "field_time is required", ch.Errors()[1].Error())
 }
